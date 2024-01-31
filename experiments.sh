106f270734f1a0555697ba86f67242123eb4631a
dr4thmos <thomas.cecconello@unimib.it>
2024-01-31 15:54:26 +0100

    some stuffs

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,16 @@
+
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__minmax__byol_banner_aug_mixed_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__minmax__byol_hulk_aug_minmax_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__minmax__byol_hulk_aug_mixed_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__minmax__simclr_banner_aug_minmax_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__minmax__simclr_banner_aug_mixed_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__mixed__byol_banner_aug_mixed_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__mixed__byol_hulk_aug_minmax_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__mixed__byol_hulk_aug_mixed_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__mixed__simclr_banner_aug_minmax_model_resnet18__K10__balanced_fixed__info__.yaml
+python3 main_kfold_crossval.py --config-path scripts/kfold/linear/robin --config-name __linear__robin__mixed__simclr_banner_aug_mixed_model_resnet18__K10__balanced_fixed__info__.yaml
+
+: '
 python3 main_linear.py --config-path scripts/finetune/robin --config-name byol_banner_minmax.yaml
 python3 main_linear.py --config-path scripts/finetune/robin --config-name byol_banner_mixed.yaml
 python3 main_linear.py --config-path scripts/finetune/robin --config-name byol_imagenet_minmax.yaml
@@ -5,7 +18,7 @@ python3 main_linear.py --config-path scripts/finetune/robin --config-name byol_i
 python3 main_linear.py --config-path scripts/finetune/robin --config-name mae_banner_minmax.yaml
 python3 main_linear.py --config-path scripts/finetune/robin --config-name mae_banner_mixed.yaml
 
-: '
+
 python3 main_linear.py --config-path scripts/linear/rgz --config-name byol_banner_minmax.yaml
 python3 main_linear.py --config-path scripts/linear/rgz --config-name byol_banner_mixed.yaml
 python3 main_linear.py --config-path scripts/linear/rgz --config-name byol_imagenet_minmax.yaml
--- generate_eval_experiments.sh
+++ generate_eval_experiments.sh
@@ -1,6 +1,23 @@
-python generate_eval_experiments.py --run_id rfkiis97 --dataset robin \
-    --dataset_path ../2-ROBIN --devices 0 --num_workers 16 --datalist info.json \
+python generate_eval_experiments.py --run_id wgoatix7 --dataset robin \
+    --dataset_path ../2-ROBIN --devices 0 --num_workers 8 --datalist info.json \
     --balancing_strategy balanced_fixed --sample_size 1650 --K 10 --reps 1
+
+python generate_eval_experiments.py --run_id uja9qvb7 --dataset robin \
+    --dataset_path ../2-ROBIN --devices 0 --num_workers 8 --datalist info.json \
+    --balancing_strategy balanced_fixed --sample_size 1650 --K 10 --reps 1
+
+python generate_eval_experiments.py --run_id uiaz3umx --dataset robin \
+    --dataset_path ../2-ROBIN --devices 0 --num_workers 8 --datalist info.json \
+    --balancing_strategy balanced_fixed --sample_size 1650 --K 10 --reps 1
+
+python generate_eval_experiments.py --run_id hljzzxvp --dataset robin \
+    --dataset_path ../2-ROBIN --devices 0 --num_workers 8 --datalist info.json \
+    --balancing_strategy balanced_fixed --sample_size 1650 --K 10 --reps 1
+
+python generate_eval_experiments.py --run_id jymmsi6s --dataset robin \
+    --dataset_path ../2-ROBIN --devices 0 --num_workers 8 --datalist info.json \
+    --balancing_strategy balanced_fixed --sample_size 1650 --K 10 --reps 1
+
 : '
 python generate_eval_experiments.py --run_id jymmsi6s --devices 3 --num_workers 16
 python generate_eval_experiments.py --run_id hkzsz08s --devices 3 --num_workers 16
